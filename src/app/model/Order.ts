6fd064b430aada5a1d878285adb586b200dae5cf
R3ktEr <ElRepu@outlook.es>
2022-02-15 19:09:56 +0100

    Creado el modelo de Order

---
+++ src/app/model/Order.ts
@@ -0,0 +1,13 @@
+export interface Order{
+    id:number;
+    pickUpdate:Date;
+    orderDate:Date;
+    //User
+    isPayed:boolean;
+    isPickedUp:boolean;
+    finalPrice:number;
+    isReady:boolean;
+
+    //discounts
+    //list
+}
