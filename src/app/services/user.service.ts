eeb5d575d90b2cc6a302bfa0c2ae410490204dce
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2022-02-21 10:22:57 +0100

    y7

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,30 +1,110 @@
 import { Injectable } from '@angular/core';
 import {User} from '../model/User';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+import { resolve } from 'dns';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HTTP) { }
 
-  public addUser(user:User){
 
+  public  addUser(user:User):Promise<User[]>{
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/user", {}, {}).then(response=>{
+        try{
+          console.log(response)
+
+          let users:User[]=JSON.parse(response.data)
+          
+          resolve(users)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
   }
 
-  public getUserByMail(){
+  public getUserByMail(all?):Promise<User[]>{
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/user", {}, {}).then(response=>{
+        try{
+          console.log(response)
 
+          let users:User[]=JSON.parse(response.data)
+          
+          resolve(users)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
   }
 
-  public getUsers(){
+  public async getUsers():Promise<User[]>{
+    
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/user", {}, {}).then(response=>{
+        try{
+          console.log(response)
 
+          let users:User[]=JSON.parse(response.data)
+          
+          resolve(users)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
+
+
+   
   }
 
-  public deleteUser(){
+  public deleteUser(id:number):Promise<User[]>{
+    let find:boolean=false;
 
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/user", {}, {}).then(response=>{
+        try{
+          console.log(response)
+
+          let users:User[]=JSON.parse(response.data)
+          while(find==false){
+
+          }
+          for (let i = 0; i < users.length; i++) {
+            if(users[i]==){
+
+            }
+            
+          }
+          resolve(users)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
   }
 
 
-  public updateUser(){
+  public updateUser():Promise<User[]>{
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/user", {}, {}).then(response=>{
+        try{
+          console.log(response)
 
+          let users:User[]=JSON.parse(response.data)
+          
+          resolve(users)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
   }
+
 }
