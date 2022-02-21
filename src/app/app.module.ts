e1c3a4352a7ca68ff736230cd2fd2994f178d88c
R3ktEr <ElRepu@outlook.es>
2022-02-21 08:43:56 +0100

    Servicio de prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [HTTP,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+import { Order } from '../model/Order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  constructor(private http: HTTP) { }
 
-  constructor() { }
+  public async getAllOrders():Promise<Order[]> {
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/orders", {}, {}).then(response=>{
+        try{
+          console.log(response)
+
+          let orders:Order[]=JSON.parse(response.data)
+          
+          resolve(orders)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
+  }
 }
