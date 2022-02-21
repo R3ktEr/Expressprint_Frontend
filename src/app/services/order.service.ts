a17ab807025e44d02d3af6cf3d9517f59a84ffc3
R3ktEr <ElRepu@outlook.es>
2022-02-21 09:30:08 +0100

    Terminado el servicion de order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -23,4 +23,96 @@ export class OrderService {
       })
     })
   }
+
+  public async getOrderById(id_user:number, id_order:number):Promise<Order> {
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/orders/"+id_user+"/"+id_order, {}, {}).then(response=>{
+        try{
+          console.log(response)
+
+          let order:Order=JSON.parse(response.data)
+          
+          resolve(order)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
+  }
+
+  public async getOrdersByUser(id_user:number):Promise<Order[]> {
+    return new Promise((resolve, reject)=>{
+      this.http.get("http://localhost:8080/orders/"+id_user, {}, {}).then(response=>{
+        try{
+          console.log(response)
+
+          let orders:Order[]=JSON.parse(response.data)
+          
+          resolve(orders)
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
+  }
+
+  public async saveOrder(order:Order):Promise<Order> {
+    return new Promise((resolve, reject)=>{
+      try{
+        let orderJSON:any=JSON.stringify(order)
+
+        this.http.post("http://localhost:8080/orders", orderJSON, {}).then(response=>{
+          try{
+            console.log(response)
+  
+            let order:Order=JSON.parse(response.data)
+            
+            resolve(order)
+          }catch(err){
+            reject(err)
+          }
+        })
+      }catch(err){
+        reject(err)
+      }
+    })
+  }
+
+  public async updateOrder(order:Order):Promise<Order> {
+    return new Promise((resolve, reject)=>{
+      try{
+        let orderJSON:any=JSON.stringify(order)
+
+        this.http.put("http://localhost:8080/orders", orderJSON, {}).then(response=>{
+          try{
+            console.log(response)
+  
+            let order:Order=JSON.parse(response.data)
+            
+            resolve(order)
+          }catch(err){
+            reject(err)
+          }
+        })
+      }catch(err){
+        reject(err)
+      }
+    })
+  }
+
+  public async deleteOrderById(id_user:number, id_order:number):Promise<string> {
+    return new Promise((resolve, reject)=>{
+      this.http.delete("http://localhost:8080/orders/"+id_user+"/"+id_order, {}, {}).then(response=>{
+        try{
+          console.log("Pedido borrado")
+
+          let orders:Order[]=JSON.parse(response.data)
+          
+          resolve("Pedido borrado")
+        }catch(err){
+          reject(err)
+        }
+      })
+    })
+  }
 }
