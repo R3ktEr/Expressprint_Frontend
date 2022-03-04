cd55fb98a87d446e78489c8ab812c6ea215b765e
R3ktEr <ElRepu@outlook.es>
2022-03-04 18:56:20 +0100

    Reseteo de tab3.html a version anterior. (Reventaba por todos lados)

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -8,23 +8,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class Tab3Page {
 public orderForm:FormGroup
-  constructor(public formBuilder: FormBuilder) {
-    this.orderForm = this.formBuilder.group({
-      nCopies:['',Validators.compose([Validators.required])],
-      isColor:['',Validators.compose([Validators.required])],
-      size:['',Validators.compose([Validators.required])],
-      thickness:['',Validators.compose([Validators.required])],
-      isTwoSides:['',Validators.compose([Validators.required])],
-      finishType:['',Validators.compose([Validators.required])],
-      impressionPerSide:['',Validators.compose([Validators.required])],
-      isVertical:['',Validators.compose([Validators.required])],
-      ringedPosition:['',Validators.compose([Validators.required])]
-    })
-  }
-
-  addOrder(){
-    console.log(this.orderForm.value);
-  }
+  constructor() {}
   
-
 }
