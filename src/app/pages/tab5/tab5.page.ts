718f9d02744fdcefda1a34b7e15367b472d814af
R3ktEr <ElRepu@outlook.es>
2022-03-08 11:26:34 +0100

    Formulario de pedido y de documento completamente funcional

--- src/app/pages/tab5/tab5.page.ts
+++ src/app/pages/tab5/tab5.page.ts
@@ -9,6 +9,7 @@ import { NewDocumentPage } from '../new-document/new-document.page';
 import { Order } from 'src/app/model/Order';
 import { formatDate } from '@angular/common';
 import { Document } from 'src/app/model/Document';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-tab5',
@@ -24,13 +25,14 @@ export class Tab5Page implements OnInit {
 
   private pickupDate:string;
   private orderDate:string;
+  private date:string;
 
   public dateTime:string;
   public userDocuments:Document[];
   public finalPrice:number;
 
   constructor(private authS:AuthService, private notS:NotificationsService, private modalController: ModalController, 
-    @Inject(LOCALE_ID) private locale: string,) {
+    @Inject(LOCALE_ID) private locale: string, private orderService:OrderService) {
     this.userDocuments=new Array()
     this.finalPrice=0;
     this.pickupDate="";
@@ -63,8 +65,9 @@ export class Tab5Page implements OnInit {
   }
 
   async formatDate(value: string) {
-    let date= format(parseISO(value), 'dd-MM-yyyy HH:mm');
-    this.pickupDate=date;
+    console.log(value)
+    this.pickupDate=formatDate(Date.now(), "YYYY-MM-ddTHH:mm:ss", this.locale)
+    this.date= format(parseISO(value), 'dd-MM-yyyy HH:mm');
 
     console.log(this.pickupDate)
   }
@@ -81,9 +84,11 @@ export class Tab5Page implements OnInit {
     modal.onDidDismiss()
       .then((data) => {
         let document:Document= data['data'];
-        
-        this.userDocuments.push(document)
-        
+
+        if(document){
+          this.userDocuments.push(document)
+        }
+      
         this.calculateFinalPrice()
     });
 
@@ -93,6 +98,22 @@ export class Tab5Page implements OnInit {
   async confirmOrder() {
     if(await this.notS.presentAlertConfirm("Confirmacion", "¿Confirmar pedido?", "Si", "No")){
       //Crear el objeto order y subirlo a la base de datos
+      let order:Order={
+        finalPrice:this.finalPrice,
+        orderDate:this.orderDate,
+        payed:false,
+        pickedUp:false,
+        pickupDate:this.pickupDate,
+        ready:false,
+        user:this.user,
+        documents:this.userDocuments
+      }
+
+      console.log(order)
+
+      await this.orderService.createOrder(order).toPromise()
+
+      console.log("Pedido subido")
     }
   }
 }
